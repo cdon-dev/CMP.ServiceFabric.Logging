80b50540d77ae6d745035d6b2023d49233c5df55
Per Ökvist <perokvist@users.noreply.github.com>
2020-03-18 11:16:03 +0100

    sample init

---
+++ samples/ConsoleSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System;
+using CMP.ServiceFabric.Logging;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Linq;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var telemetryConfig = TelemetryConfiguration.CreateDefault();
+            telemetryConfig.InstrumentationKey = args.First();
+
+            var serilogLogger = new LoggerConfiguration()
+                .DefaultCmp(telemetryConfig, "development")
+                .CreateLogger();
+            
+            Log.Logger = serilogLogger;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
